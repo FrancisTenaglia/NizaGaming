a42a70a540cf4b9a14818700ec544c1091cbe56b
FrancisTenaglia <fgtenaglia@alumnos.fi.mdp.edu.ar>
2023-10-17 10:39:41 -0300

    probando imagenes

--- src/Components/homeImage.jsx
+++ src/Components/homeImage.jsx
@@ -1,20 +1,78 @@
-import fondo from "./images/fondo.png"
+import fondo from "./images/img4.png"
+import imgSlots from "./images/img3.jpg"
+import imgDeportivas from "./images/img2.png"
+import imgRuleta from "./images/img1.jpg"
 
 const Promociones = () => {
     return( 
-        <div className="row py-3 mt--1" style={{backgroundColor:"black" }}>   
-            <div className="container d-flex p-4 justify-content-center">
-                <p className="text-light" style={{fontFamily:"unset", fontSize:"20px"}}>PLATAFORMA UNIVERSAL DISEÑO 2024</p>
+        <div className="row py-3 d-flex justify-content-center">
 
-            </div>
-            <div className="row">
-                <img src={fondo} alt="fondo"/>
+            <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"white" }}>   
+                
+                <div className="row">
+                    <img src={fondo} alt="fondo"/>
+                </div>  
+
+                <div className="container d-flex p-4 justify-content-center">
+                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
+                </div>
+            </div>    
+
+             <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"BLACK" }}>   
+                <div className="container d-flex p-4 justify-content-center">
+                    <p className="text-white" style={{fontFamily:"unset", fontSize:"30px"}}>DEPORTIVAS</p>
+                </div>
+                <div className="container">
+                    <div className="text-white py-2 col text-center text-justify">
+                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <img src={imgDeportivas} alt="fondo"/>
+                </div>  
+
+                <div className="container d-flex p-4 justify-content-center">
+                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
+                </div>
+            </div>    
+
+            <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"white" }}>   
+                <div className="container d-flex p-4 justify-content-center">
+                    <p className="text-dark" style={{fontFamily:"unset", fontSize:"30px"}}>SLOTS</p>
+                </div>
+                <div className="container">
+                    <div className="text-dark py-2 col text-center text-justify">
+                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <img src={imgSlots} alt="fondo"/>
+                </div>  
+
+                <div className="container d-flex p-4 justify-content-center">
+                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
+                </div>
             </div>  
 
-            <div className="container d-flex p-4 justify-content-end">
-                <a href="/" style={{textDecoration:"none"}}>Más información... </a>
-            </div>
-        </div>        
+            <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"BLACK" }}>   
+                <div className="container d-flex p-4 justify-content-center">
+                    <p className="text-white" style={{fontFamily:"unset", fontSize:"30px"}}>CASINO EN VIVO</p>
+                </div>
+                <div className="container">
+                    <div className="text-white py-2 col text-center text-justify">
+                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <img src={imgRuleta} alt="fondo"/>
+                </div>  
+
+                <div className="container d-flex p-4 justify-content-center">
+                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
+                </div>
+            </div>  
+            
+        </div>
     );
 };
 
