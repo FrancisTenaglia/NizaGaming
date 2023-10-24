a9e1435fcfddd0f075239687b337c85ea897a9e4
FrancisTenaglia <fgtenaglia@alumnos.fi.mdp.edu.ar>
2023-10-24 19:25:11 -0300

    acomode un poco el codigo y las imagenes

--- src/Components/homeImage.jsx
+++ src/Components/homeImage.jsx
@@ -1,77 +1,37 @@
-import fondo from "./images/img4.png"
-import imgSlots from "./images/img3.jpg"
-import imgDeportivas from "./images/img2.png"
-import imgRuleta from "./images/img1.jpg"
+import fondo from "../images/img4.png"
+import { homes } from "../info"
 
 const Promociones = () => {
     return( 
         <div className="row  d-flex justify-content-center" style={{marginRight:"-15px", marginLeft:"-15px"}}>
-
             <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"white" }}>   
-                
-                <div className="row">
-                    <img src={fondo} alt="fondo"/>
+                <div className="row col-md-6">
+                        <img src={fondo} alt="fondo"/>
                 </div>  
-
                 <div className="container d-flex p-4 justify-content-center">
-        
                 </div>
             </div>    
 
-             <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"BLACK" }}>   
-                <div className="container d-flex p-4 justify-content-center">
-                    <p className="text-white" style={{fontFamily:"unset", fontSize:"30px"}}>DEPORTIVAS</p>
-                </div>
-                <div className="container">
-                    <div className="text-white py-2 col text-center text-justify">
-                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+            {homes.map(({colorFondo, textColor, titulo, texto, imagen } ) => (
+
+                <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor: colorFondo }}>   
+                    <div className="container d-flex p-4 justify-content-center">
+                        <p className={textColor} style={{fontFamily:"unset", fontSize:"30px"}}>{titulo}</p>
                     </div>
-                </div>
-                <div className="row">
-                    <img src={imgDeportivas} alt="fondo"/>
-                </div>  
-
-                <div className="container d-flex p-4 justify-content-center">
-                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
-                </div>
-            </div>    
-
-            <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"#ededed" }}>   
-                <div className="container d-flex p-4 justify-content-center">
-                    <p className="text-dark" style={{fontFamily:"unset", fontSize:"30px"}}>SLOTS</p>
-                </div>
-                <div className="container">
-                    <div className="text-dark py-2 col text-center text-justify">
-                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+                    <div className="container">
+                        <div className="py-2 col text-center text-justify">
+                            <p className={textColor} style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>{texto}</p>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <img src={imgSlots} alt="fondo"/>
-                </div>  
+                    <div className="row col-md-6">
+                        <img src={imagen} alt="fondo"/>
+                    </div>  
 
-                <div className="container d-flex p-4 justify-content-center">
-                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
-                </div>
-            </div>  
-
-            <div className="row py-3 d-flex p-4 justify-content-center " style={{backgroundColor:"BLACK" }}>   
-                <div className="container d-flex p-4 justify-content-center">
-                    <p className="text-white" style={{fontFamily:"unset", fontSize:"30px"}}>CASINO EN VIVO</p>
-                </div>
-                <div className="container">
-                    <div className="text-white py-2 col text-center text-justify">
-                        <p style={{fontFamily:"sans-serif", fontSize:"12px", margin:"0"}}>Más de 5.000 juegos fascinantes disponibles en suite <br></br>de alta velocidad, máxima calidad gráfica y ostentosos premios <br></br>de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...</p>
+                    <div className="container d-flex p-4 justify-content-center">
+                        <a href="/" style={{textDecoration:"none"}}>Más información... </a>
                     </div>
-                </div>
-                <div className="row">
-                    <img src={imgRuleta} alt="fondo"/>
-                </div>  
-
-                <div className="container d-flex p-4 justify-content-center">
-                    <a href="/" style={{textDecoration:"none"}}>Más información... </a>
-                </div>
-            </div>  
-            
+                </div>    
+            ))}
         </div>
     );
 };
---
+++ src/info.js
@@ -0,0 +1,24 @@
+export const homes = [
+    {
+      colorFondo: "black",
+      textColor: "text-white",
+      titulo: "DEPORTIVAS",
+      texto: "Más de 5.000 juegos fascinantes disponibles en suite de alta velocidad, máxima calidad gráfica y ostentosos premios de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...",
+      imagen: "img2.png"
+    },
+    {
+        colorFondo: "#ededed",
+        textColor: "text-black",
+        titulo: "SLOTS",
+        texto: "Más de 5.000 juegos fascinantes disponibles en suite de alta velocidad, máxima calidad gráfica y ostentosos premios de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...",
+        imagen: "img3.jpg"
+    },
+    {
+        colorFondo: "black",
+        textColor: "text-white",
+        titulo: "CASINO EN VIVO",
+        texto: "Más de 5.000 juegos fascinantes disponibles en suite de alta velocidad, máxima calidad gráfica y ostentosos premios de la mano de los mejores proveedores del mundo. Pragmatic Play, Ruby, Bongoo, Ka Gaming ...",
+        imagen: "../img1.jpg"
+    }
+
+];
