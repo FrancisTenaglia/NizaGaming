93457bba9c5cdf73d7bee69b822adaaa91242cee
FrancisTenaglia <fgtenaglia@alumnos.fi.mdp.edu.ar>
2023-11-14 21:17:50 -0300

    aa

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer =() =>{
     return(
-        <div className="container">
-            <footer className="">
+        <div className="container" style={{position:"relative"}}>
                 
                 <div className="d-flex flex-column flex-sm-row justify-content-between pt-3" style={{textAlign:"justify"}}>
                     <p className="text-secondary" style={{fontFamily:"Arimo"}}>© Techvantage 2023. Todos los derechos reservados. Nuestra empresa se dedica a proporcionar soluciones de vanguardia para casinos en línea, garantizando experiencias de juego seguras y emocionantes. Con un enfoque en la innovación y la excelencia, nos esforzamos por brindar tecnología de punta para la industria del entretenimiento digital. Ponte en contacto con nosotros para descubrir cómo podemos ayudarte a crear la mejor plataforma de casino online para tu negocio.</p>
@@ -66,7 +65,6 @@ const Footer =() =>{
                 <div className="d-flex flex-column flex-sm-row justify-content-between py-1">
                     <p >Privacy Policy | Terms of Use | Sales | Legal | Site Map</p>
                 </div>
-            </footer>
         </div>
     );
 };
--- src/Components/homeImage.jsx
+++ src/Components/homeImage.jsx
@@ -2,22 +2,19 @@ import { homes } from "../info"
 
 const Promociones = () => {
     return( 
-        <div className="row  d-flex justify-content-center" style={{marginRight:"-15px", marginLeft:"-15px"}}>
+        <div className="row  d-flex justify-content-center" style={{marginRight:"-15px", marginLeft:"-15px",}}>
             {homes.map(({colorFondo, textColor, titulo, texto, imagen } ) => (
 
-                <div className="row mt-3 pt-4 d-flex justify-content-center"  style={{backgroundColor: colorFondo }}>   
+                <div className="row pt-4 d-flex justify-content-center"  style={{backgroundColor: colorFondo }}>   
                     <div className="container">
                         <div className="py-2 col text-center text-justify">
                         <h1 className={textColor} style={{fontFamily:"Albert sans", fontSize:"35px", }}>{titulo}</h1>
                             <p className={textColor} style={{fontFamily:"", fontSize:"18px", margin:"0"}}>{texto}</p>
                         </div>
                     </div>
-                    <div className="row col-md-6 pt-4">
-                        <img src={imagen} alt="fondo"/>
+                    <div className="row col-md-6 pt-4" >
+                        <img src={imagen} alt="fondo" style={{transform:"scale(0.8)", marginBottom:"40px"}}/>
                     </div>  
-
-                    <div className="row" style={{height:"110px"}}></div>
-
 
                 </div>    
             ))}
